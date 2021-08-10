847157edfe15acbbf7538a7be275964bd1acb6e8
Muthu Mathiyazhagan <muthumathiyazhagan@gmail.com>
2021-08-10 19:13:39 +0530

    Test web api demo for ppt

---
+++ TestWebApi/Models/Products.cs
@@ -0,0 +1,16 @@
+using System;
+namespace TestWebApi.Models
+{
+    public class Products
+    {
+        public string Code { get; set; }
+        public string Description { get; set; }
+        public string Um { get; set; }
+        public string CodStat { get; set; }
+        public int PcCart { get; set; }
+        public double NetWeight { get; set; }
+        public string State { get; set; }
+        public DateTime CreationDate { get; set; }
+        public double Price { get; set; }
+    }
+}
